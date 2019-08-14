fcbf8ddcc0ef72e1651e4955607f3ce2d052ca51
Samrat Jha <samrat.jha@appian.com>
2019-08-13 22:05:46 -0400

    Service discovery

--- accountservice/model/account.go
+++ accountservice/model/account.go
@@ -1,5 +1,7 @@
 package model
+
 type Account struct {
-    Id string `json:"id"`
-    Name string  `json:"name"`
+	Id       string `json:"id"`
+	Name     string `json:"name"`
+	ServedBy string `json:"servedBy"`
 }
--- accountservice/service/handlers.go
+++ accountservice/service/handlers.go
@@ -1,12 +1,14 @@
 package service
 
 import (
-	"net/http"
-	"github.com/gorilla/mux"
 	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
 	"strconv"
-  "github.com/samratjha96/Go-Microservice/accountservice/dbclient"
-  "fmt"
+
+	"github.com/gorilla/mux"
+	"github.com/samratjha96/Go-Microservice/accountservice/dbclient"
 )
 
 var DBClient dbclient.IBoltClient
@@ -16,17 +18,20 @@ func GetAccount(w http.ResponseWriter, r *http.Request) {
 	// Read the 'accountId' path parameter from the mux map
 	var accountId = mux.Vars(r)["accountId"]
 
-  // Read the account struct BoltDB
+	// Read the account struct BoltDB
 	account, err := DBClient.QueryAccount(accountId)
 
-  // If err, return a 404
+	// If err, return a 404
 	if err != nil {
-    fmt.Println("Some error occured serving " + accountId + ": " + err.Error())
+		fmt.Println("Some error occured serving " + accountId + ": " + err.Error())
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-  // If found, marshal into JSON, write headers and content
+	// Update account struct with the service IP that is handling this request
+	account.ServedBy = getIp()
+
+	// If found, marshal into JSON, write headers and content
 	data, _ := json.Marshal(account)
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Content-Length", strconv.Itoa(len(data)))
@@ -34,38 +39,54 @@ func GetAccount(w http.ResponseWriter, r *http.Request) {
 	w.Write(data)
 }
 
+func getIp() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "error"
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback then display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	panic("Unable to determine local IP address (non loopback). Exiting.")
+}
+
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
-  // Since we're here, we already know that HTTP service is up. Let's just check the state of the boltdb connection
-  dbUp := DBClient.Check()
-  if dbUp && isHealthy {
-    data, _ := json.Marshal(healthCheckResponse{Status: "UP"})
-    writeJsonResponse(w, http.StatusOK, data)
-  } else {
-      data, _ := json.Marshal(healthCheckResponse{Status: "Database unaccessible"})
-      writeJsonResponse(w, http.StatusServiceUnavailable, data)
-  }
+	// Since we're here, we already know that HTTP service is up. Let's just check the state of the boltdb connection
+	dbUp := DBClient.Check()
+	if dbUp && isHealthy {
+		data, _ := json.Marshal(healthCheckResponse{Status: "UP"})
+		writeJsonResponse(w, http.StatusOK, data)
+	} else {
+		data, _ := json.Marshal(healthCheckResponse{Status: "Database unaccessible"})
+		writeJsonResponse(w, http.StatusServiceUnavailable, data)
+	}
 }
 func writeJsonResponse(w http.ResponseWriter, status int, data []byte) {
-  w.Header().Set("Content-Type", "application/json")
-  w.Header().Set("Content-Length", strconv.Itoa(len(data)))
-  w.WriteHeader(status)
-  w.Write(data)
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Length", strconv.Itoa(len(data)))
+	w.WriteHeader(status)
+	w.Write(data)
 }
 
 type healthCheckResponse struct {
-  Status string `json:"status"`
+	Status string `json:"status"`
 }
 
-
 var isHealthy = true
+
 func SetHealthyState(w http.ResponseWriter, r *http.Request) {
-        var state, err = strconv.ParseBool(mux.Vars(r)["state"])
-        // If we couldn't parse the state param, return a HTTP 400
-        if err != nil {
-          fmt.Println("Invalid request to SetHealthyState, allowed values are true or false")
-          w.WriteHeader(http.StatusBadRequest)
-          return
-        }
-        isHealthy = state
-        w.WriteHeader(http.StatusOK)
+	var state, err = strconv.ParseBool(mux.Vars(r)["state"])
+	// If we couldn't parse the state param, return a HTTP 400
+	if err != nil {
+		fmt.Println("Invalid request to SetHealthyState, allowed values are true or false")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	isHealthy = state
+	w.WriteHeader(http.StatusOK)
 }
